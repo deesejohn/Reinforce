0ad2788211fb0a908a87de5594b1db7d133bc6ff
deesejohn <deese.john@gmail.com>
2019-08-24 16:04:35 -0500

    Add ISObjectRichTextImageRetrieve #14

---
+++ src/Reinforce/RestApi/ISObjectRichTextImageRetrieve.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using RestEase;
+
+namespace Reinforce.RestApi
+{
+    /// <summary>
+    /// SObject Rich Text Image Retrieve
+    /// Retrieves the specified image data from a specific rich text area field in a given record.
+    /// https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_sobject_rich_text_image_retrieve.htm
+    /// </summary>
+    public interface ISObjectRichTextImageRetrieve
+    {
+        [Get("/services/data/v46.0/sobjects/{sObjectName}/{id}/richTextImageFields/{fieldName}/{contentReferenceId}")]
+        [Header("Authorization", "Bearer")]
+        Task<Stream> GetAsync(
+            [Path] string sObjectName,
+            [Path] string id,
+            [Path] string fieldName,
+            [Path] string contentReferenceId,
+            CancellationToken cancellationToken = default(CancellationToken)
+        );
+    }
+}
