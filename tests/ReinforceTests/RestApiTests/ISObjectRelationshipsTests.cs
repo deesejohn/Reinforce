70044917a9bdced39765767fc8e7f3b57e9df768
deesejohn <deese.john@gmail.com>
2019-08-25 16:06:58 -0500

    Added ISObjectRelationships #15 (#73)

---
+++ tests/ReinforceTests/RestApiTests/ISObjectRelationshipsTests.cs
@@ -0,0 +1,49 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Reinforce.RestApi;
+using Reinforce.RestApi.Models;
+using Xunit;
+
+namespace ReinforceTests.RestApiTests
+{
+    public class ISObjectRelationshipsTests
+    {
+        [Theory, AutoData]
+        public async Task ISObjectRelationships(
+            QueryResponse<string> expected,
+            string sObjectName,
+            string id,
+            string relationshipFieldName
+        )
+        {
+            using(var handler = MockHttpMessageHandler.SetupHandler(expected))
+            {
+                var api = handler.SetupApi<ISObjectRelationships>();
+                var result = await api.GetAsync<string>(sObjectName, id, relationshipFieldName, CancellationToken.None);
+                result.Should().BeEquivalentTo(expected);
+                handler.ConfirmPath($"/services/data/v46.0/sobjects/{sObjectName}/{id}/{relationshipFieldName}");
+            }
+        }
+
+        [Theory, AutoData]
+        public async Task ISObjectRelationships_Fields(
+            QueryResponse<string> expected,
+            string sObjectName,
+            string id,
+            string relationshipFieldName,
+            string[] fields
+        )
+        {
+            using(var handler = MockHttpMessageHandler.SetupHandler(expected))
+            {
+                var api = handler.SetupApi<ISObjectRelationships>();
+                var result = await api.GetAsync<string>(sObjectName, id, relationshipFieldName, string.Join(",", fields), CancellationToken.None);
+                result.Should().BeEquivalentTo(expected);
+                handler.ConfirmPath($"/services/data/v46.0/sobjects/{sObjectName}/{id}/{relationshipFieldName}?fields={string.Join("%2C", fields)}");
+            }
+        }
+    }
+}
