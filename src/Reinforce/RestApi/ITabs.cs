cedf2eafc901e09e3d7b2838bca8d65af2790242
deesejohn <deese.john@gmail.com>
2019-09-03 22:57:32 -0500

    Added ITabs #53 (#83)

---
+++ src/Reinforce/RestApi/ITabs.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Reinforce.RestApi.Constants;
+using Reinforce.RestApi.Models;
+using RestEase;
+
+namespace Reinforce.RestApi
+{
+    /// <summary>
+    /// Tabs
+    /// Returns a list of all tabs—including Lightning page tabs—available to the current user, 
+    /// regardless of whether the user has chosen to hide tabs via the All Tabs (+) tab 
+    /// customization feature.
+    /// https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_tabs.htm
+    /// </summary>
+    public interface ITabs
+    {
+        [Get("/services/data/{version}/tabs")]
+        [Header("Authorization", "Bearer")]
+        Task<IEnumerable<Tab>> GetAsync(
+            CancellationToken cancellationToken = default(CancellationToken),
+            [Path] string version = Api.Version
+        );
+    }
+}
---
+++ tests/ReinforceTests/RestApiTests/ITabsTests.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Reinforce.RestApi;
+using Reinforce.RestApi.Constants;
+using Reinforce.RestApi.Models;
+using Xunit;
+
+namespace ReinforceTests.RestApiTests
+{
+    public class ITabsTests
+    {
+        [Theory, AutoData]
+        public async Task ITabs(IEnumerable<Tab> expected)
+        {
+            using(var handler = MockHttpMessageHandler.SetupHandler(expected))
+            {
+                var api = handler.SetupApi<ITabs>();
+                var result = await api.GetAsync(CancellationToken.None, "v44.0");
+                result.Should().BeEquivalentTo(expected);
+                handler.ConfirmPath($"/services/data/v44.0/tabs");
+            }            
+        }
+
+        [Theory, AutoData]
+        public async Task ITabs_ApiVersion(IEnumerable<Tab> expected)
+        {
+            using(var handler = MockHttpMessageHandler.SetupHandler(expected))
+            {
+                var api = handler.SetupApi<ITabs>();
+                var result = await api.GetAsync(CancellationToken.None);
+                result.Should().BeEquivalentTo(expected);
+                handler.ConfirmPath($"/services/data/{Api.Version}/tabs");
+            }            
+        }
+    }
+}
