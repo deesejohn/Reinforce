0a054aafdfcfe931bcee4c9e6dcb17e3cf12e210
deesejohn <deese.john@gmail.com>
2019-09-17 17:37:10 -0500

    Added Docs

--- src/Reinforce/RestApi/ISearch.cs
+++ src/Reinforce/RestApi/ISearch.cs
@@ -6,6 +6,11 @@ using RestEase;
 
 namespace Reinforce.RestApi
 {
+    /// <summary>
+    /// Search
+    /// Executes the specified SOSL search. The search string must be URL-encoded.
+    /// https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_search.htm
+    /// </summary>
     public interface ISearch
     {
         [Get("/services/data/{version}/search")]
